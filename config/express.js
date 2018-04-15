c54ea321d2c7adbff6d360de6569008a1faf7070
shishir99111 <shishir99111@gmail.com>
2018-04-15 18:39:04 +0530

    responding browser for favicon & robots.txt

--- config/express.js
+++ config/express.js
@@ -12,5 +12,8 @@ module.exports = () => {
   app.set('etag', false);
 
   app.use(express.static('./view'));
+
+  app.get(['/favicon.ico', '/robot.txt'], (req, res) => { res.status(204); });
+
   return app;
 };
